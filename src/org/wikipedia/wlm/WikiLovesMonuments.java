2317e92afc36975e3091a2a05936e5c0284f1508
Brion Vibber <brion@pobox.com>
2012-05-24 18:26:56 -0700

    Basic attempt to catch pinch-zoom style events from the Java side on Android 2.x
    Seems to work on my Nexus 1. Doesn't yet do anything but log the distance delta.
    Should break out into a PhoneGap plugin and give it some way to pass data back
    to the PhoneGap side.

--- src/org/wikipedia/wlm/WikiLovesMonuments.java
+++ src/org/wikipedia/wlm/WikiLovesMonuments.java
@@ -2,15 +2,63 @@ package org.wikipedia.wlm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
 import org.apache.cordova.*;
 
-public class WikiLovesMonuments extends DroidGap
+public class WikiLovesMonuments extends DroidGap implements OnTouchListener
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        appView.setOnTouchListener(this);
+    }
+
+    private float zoomInitialDist, zoomFinalDist;
+    private boolean zoomActive = false;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        case MotionEvent.ACTION_POINTER_DOWN:
+        	zoomActive = true;
+        	zoomInitialDist = fingerDistance(event);
+        	zoomFinalDist = zoomInitialDist;
+        	Log.d("motion", "DOWN dist is " + zoomInitialDist);
+        	return true;
+        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_POINTER_UP:
+        	if (zoomActive) {
+        		float delta = zoomFinalDist - zoomInitialDist;
+        		Log.d("motion", "UP final delta is " + delta);
+        		zoomActive = false;
+        		return true;
+        	} else {
+        		return false;
+        	}
+        case MotionEvent.ACTION_MOVE:
+        	if (zoomActive) {
+	        	zoomFinalDist = fingerDistance(event);
+	        	Log.d("motion", "MOVE dist is " + zoomFinalDist);
+	        	return true;
+        	} else {
+        		return false;
+        	}
+        }
+        return false;
+    }
+    
+    private float fingerDistance(MotionEvent event) {
+    	float dx = event.getX(0) - event.getX(1),
+    		dy = event.getY(0) - event.getY(1),
+    		dist = FloatMath.sqrt(dx * dx + dy * dy);
+    	return dist;
     }
 }
 
